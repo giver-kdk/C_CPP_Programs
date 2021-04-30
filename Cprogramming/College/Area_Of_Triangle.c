383afa90c5ce92e2de697ae366c37dd923169bfd
Giver-Coder <givebro13@gmail.com>
2021-04-30 19:57:27 +0545

    set

--- Cprogramming/College/Area_Of_Triangle.c
+++ Cprogramming/College/Area_Of_Triangle.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 int main()
 {
@@ -9,16 +8,4 @@ int main()
     scanf("%d", &h);
     A= 0.5*b*h;
     printf("The Area of the circle is: %d", A);
-=======
-#include <stdio.h>
-int main()
-{
-    int b, h, A;                                //base and height of the triangle//
-    printf("Enter the base of triangle: ");
-    scanf("%d", &b);
-    printf("Enter the height of triangle: ");
-    scanf("%d", &h);
-    A= 0.5*b*h;
-    printf("The Area of the circle is: %d", A);
->>>>>>> 31a0cd55077ad1ba304e75cb49a460b4e3e75082
 }
--- Cprogramming/College/Return.c
+++ Cprogramming/College/Return.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 int main()
 {
@@ -21,27 +20,3 @@ int multiply(int a, int b)
 }
 
 
-=======
-#include <stdio.h>
-int main()
-{
-    int a, b, mult;
-    printf("Enter the first integer: ");
-    scanf("%d", &a);
-    printf("Enter the second integer: ");
-    scanf("%d", &b);
-    mult=multiply(a,b);
-    printf("The mulitplication is: %d", mult);
-
-   // return 0;
-}
-
-int multiply(int a, int b)
-{
-    int m;
-    m= a*b;
-  //  return m;
-}
-
-
->>>>>>> 31a0cd55077ad1ba304e75cb49a460b4e3e75082
